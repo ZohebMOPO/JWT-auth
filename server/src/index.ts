6542d7b2f07a3a6f6fa8812349bafc9ade75aadd
Zoheb Alli Khan <understandingzak@gmail.com>
2021-08-04 10:22:48 +0530

    Express Server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,25 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import express from "express";
+import { createConnection } from "typeorm";
+import { User } from "./entity/User";
 
-createConnection().then(async connection => {
+const app = express();
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+(() => {})();
+// createConnection().then(async connection => {
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+//     console.log("Inserting a new user into the database...");
+//     const user = new User();
+//     user.firstName = "Timber";
+//     user.lastName = "Saw";
+//     user.age = 25;
+//     await connection.manager.save(user);
+//     console.log("Saved a new user with id: " + user.id);
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+//     console.log("Loading users from the database...");
+//     const users = await connection.manager.find(User);
+//     console.log("Loaded users: ", users);
 
-}).catch(error => console.log(error));
+//     console.log("Here you can setup and run express/koa/any other framework.");
+
+// }).catch(error => console.log(error));
